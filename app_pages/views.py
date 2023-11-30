c38513410ffa1c64caa39da1fc088a84a902a529
Foh, Ian (UG - Comp Sci & Elec Eng) <if00225@surrey.ac.uk>
2023-11-30 15:50:45 +0000

    account page

--- app_pages/views.py
+++ app_pages/views.py
@@ -2,23 +2,27 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def home(request):
     context = {}
-    return render(request, 'app_pages/home.html',context)
+    return render(request, 'app_pages/home_view.html',context)
 
 def contact(request):
     context = {}
-    return render(request, 'app_pages/contact.html',context)
+    return render(request, 'app_pages/contact_view.html',context)
 
 def about(request):
     context = {}
-    return render(request, 'app_pages/about.html',context)
+    return render(request, 'app_pages/about_view.html',context)
 
 def login(request):
     context = {}
-    return render(request, 'app_pages/login.html',context)
+    return render(request, 'app_pages/login_view.html',context)
 
+@login_required(login_url="login")
 def account(request):
     context = {}
-    return HttpResponse("account")
+    return render(request, 'app_pages/account_view.html',context)
