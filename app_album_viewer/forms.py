15e83fe215987d1ec08dda5e6dbfe3ddc05a12c3
Ian Foh <ian.foh@gmail.com>
2023-12-08 01:15:08 +0000

    penultimate

--- app_album_viewer/forms.py
+++ app_album_viewer/forms.py
@@ -4,16 +4,34 @@ from .models import *
 class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
-        fields = ['title', 'description']
+        fields = ['cover', 'title', 'description', 'artist', 'price', 'format', 'release_date']
+
         widgets = {
+            'cover': forms.FileInput(attrs={
+                'class': 'formfield',
+            }),
             'title': forms.TextInput(attrs={
                 'class': 'formfield',
                 'placeholder': 'Album Title',
             }),
             'description': forms.Textarea(attrs={
                 'class': 'formfield',
-                'placeholder': 'Album Description',
-                'rows' : 25,
-                'cols' : 60,
+                'placeholder': 'Album Description'
+            }),
+            'artist': forms.TextInput(attrs={
+                'class': 'formfield',
+                'placeholder': 'Artist',
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'formfield',
+                'placeholder': 'Price',
+            }),
+            'format': forms.Select(attrs={
+                'class': 'formfield',
+            }),
+            'release_date': forms.DateInput(attrs={
+                'class': 'formfield',
+                'placeholder': 'Release Date (YYYY-MM-DD)',
+                'style': 'width: 15%',
             }),
         }
--- app_pages/forms.py
+++ app_pages/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from .models import *
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=255)
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'style': 'width: 20%'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'style': 'width: 20%'}))
 
 class EmailForm(forms.Form):
-    recipient_email = forms.EmailField(required=True,label="To:")
-    subject = forms.CharField(required=True,label="Subject:")
-    message = forms.CharField(widget=forms.Textarea,required=True,label="Message:")
+    recipient_email = forms.EmailField(required=True,label="To:", widget=forms.EmailInput(attrs={'style': 'width: 75%'}))
+    subject = forms.CharField(required=True,label="Subject:", widget=forms.TextInput(attrs={'style': 'width: 75%'}))
+    message = forms.CharField(required=True,label="Message:", widget=forms.Textarea(attrs={'style': 'width: 75%'}))
